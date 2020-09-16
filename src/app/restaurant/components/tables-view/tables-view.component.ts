13403b94330f401df4b85c5da30a2e8e95b5d38c
Mark Whitfeld <m.whitfeld@criteo.com>
2020-09-16 09:00:02 +0200

    chore: small cleanup

--- src/app/restaurant/components/tables-view/tables-view.component.ts
+++ src/app/restaurant/components/tables-view/tables-view.component.ts
@@ -1,6 +1,4 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { map } from 'rxjs/operators';
 
 import { TableOrderViewModel } from '../../view-models/table-view-model.queries';
 
@@ -14,26 +12,5 @@ export class TablesViewComponent {
   @Input()
   tableOrders: TableOrderViewModel[] = [];
 
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 1, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 1 },
-        { title: 'Card 4', cols: 1, rows: 1 },
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() { }
 }
